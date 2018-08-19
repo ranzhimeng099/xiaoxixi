96512fd1c50222a9d26dfb77553c77f0de9935b7
ranzhimeng099 <18721860940@163.com>
2018-08-19 10:57:21 +0800

    springMVC @ResponseBody、messageConverters、Jackson测试

--- xiaoxixi-web/src/main/java/com/xxrr/home/controller/FirstSpringMvcController.java
+++ xiaoxixi-web/src/main/java/com/xxrr/home/controller/FirstSpringMvcController.java
@@ -2,9 +2,12 @@ package com.xxrr.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/zx")
@@ -13,5 +16,14 @@ public class FirstSpringMvcController {
     public void handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 
         resp.getWriter().println("test controller");
+    }
+
+    @RequestMapping("/testResponseBody.do")
+    @ResponseBody
+    public Map testResponseBody(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        Map<String,String> testMap=new HashMap<>();
+        testMap.put("s1","hello");
+        testMap.put("s2","world");
+        return testMap;
     }
 }
